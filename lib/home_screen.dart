65c6f90933c96bddb511601dcc9b66790f2c14cd
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2024-02-12 19:11:18 +0330

    Home Screen mainCard

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,8 +8,36 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  static const tabStyle = TextStyle(
+      fontSize: 15, color: Colors.blueGrey, fontWeight: FontWeight.bold);
+
   @override
   Widget build(BuildContext context) {
-    return  Container(color: Colors.green,);
+    return Card(
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10))),
+      elevation: 20,
+      margin: const EdgeInsets.all(10),
+      child: Container(
+        margin: const EdgeInsets.all(10),
+        child: const Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  "زبان مبدا",
+                  style: tabStyle,
+                ),
+                Text("زبان مقصد", style: tabStyle),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
