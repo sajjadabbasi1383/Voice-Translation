df7809f0d7272cfb54deaaf0303dd922287cce39
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2024-02-12 19:00:15 +0330

    main Drawer

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -12,29 +12,92 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: DefaultTabController(
+    return SafeArea(
+        child: DefaultTabController(
       initialIndex: 0,
       length: 2,
       child: Scaffold(
         appBar: AppBar(
-          title: const Text("مترجم آنلاین",style: TextStyle(fontSize: 17),),
-          actions: const [
-            Icon(Icons.more_vert)
-          ],
+          title: const Text(
+            "مترجم آنلاین",
+            style: TextStyle(fontSize: 17),
+          ),
+          actions: const [Icon(Icons.more_vert)],
           bottom: const TabBar(
             tabs: [
-              Tab(text: "ترجمه آنلاین",icon: Icon(Icons.g_translate_outlined),),
-              Tab(text: "تشخیص کلمات",icon: Icon(Icons.document_scanner_outlined),),
+              Tab(
+                text: "ترجمه آنلاین",
+                icon: Icon(Icons.g_translate_outlined),
+              ),
+              Tab(
+                text: "تشخیص کلمات",
+                icon: Icon(Icons.document_scanner_outlined),
+              ),
             ],
           ),
         ),
+        drawer: Drawer(
+            width: MediaQuery.sizeOf(context).width / 1.7,
+            child: ListView(
+              children: [
+                UserAccountsDrawerHeader(
+                    accountName: const Text(
+                      "مترجم آنلاین",
+                    ),
+                    accountEmail: const Text("سجاد عباسی"),
+                    currentAccountPicture: Image.asset(
+                      "assets/images/logo.jpg",
+                    )),
+                InkWell(
+                  onTap: () {},
+                  child: const ListTile(
+                    title: Text("درباره ما"),
+                    leading: Icon(Icons.info_outline),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: const ListTile(
+                    title: Text("ارتباط با ما"),
+                    leading: Icon(Icons.telegram_outlined),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: const ListTile(
+                    title: Text("ثبت نظر"),
+                    leading: Icon(Icons.star_rate_outlined),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: const ListTile(
+                    title: Text("سایر برنامه ها"),
+                    leading: Icon(Icons.app_shortcut_outlined),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: const ListTile(
+                    title: Text("اشتراک برنامه"),
+                    leading: Icon(Icons.share),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: const ListTile(
+                    title: Text("خروج"),
+                    leading: Icon(Icons.exit_to_app),
+                  ),
+                ),
+              ],
+            )),
         body: const TabBarView(
           children: [
             HomeScreen(),
             ScanScreen(),
           ],
         ),
-        drawer: const Drawer(),
       ),
     ));
   }
