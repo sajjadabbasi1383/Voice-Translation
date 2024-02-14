1e2fd15779000c42c4046989e7328ec830a3c77e
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2024-02-14 12:33:53 +0330

    Dropdown language source

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -9,7 +9,30 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   static const tabStyle = TextStyle(
-      fontSize: 15, color: Colors.blueGrey, fontWeight: FontWeight.bold);
+      fontSize: 15, color: Colors.blueGrey, fontWeight: FontWeight.bold,fontFamily: 'irs');
+
+  List<String> source = [
+    "انگلیسی",
+    "فارسی",
+    "اسپانیایی",
+    "فرانسوی",
+    "آلمانی",
+    "ایتالیایی",
+    "روسی",
+  ];
+
+  List<String> dest = [
+    "فارسی",
+    "انگلیسی",
+    "اسپانیایی",
+    "فرانسوی",
+    "آلمانی",
+    "ایتالیایی",
+    "روسی",
+  ];
+
+  String selectedItemSource = "انگلیسی";
+  String selectedItemDest = "فارسی";
 
   @override
   Widget build(BuildContext context) {
@@ -20,12 +43,12 @@ class _HomeScreenState extends State<HomeScreen> {
       margin: const EdgeInsets.all(10),
       child: Container(
         margin: const EdgeInsets.all(10),
-        child: const Column(
+        child: Column(
           children: [
-            SizedBox(
+            const SizedBox(
               height: 20,
             ),
-            Row(
+            const Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 Text(
@@ -33,6 +56,50 @@ class _HomeScreenState extends State<HomeScreen> {
                   style: tabStyle,
                 ),
                 Text("زبان مقصد", style: tabStyle),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  width: MediaQuery.sizeOf(context).width / 3,
+                  height: 50,
+                  margin: const EdgeInsets.all(16),
+                  child: DropdownButtonFormField<String>(
+                    style: tabStyle,
+                    decoration: InputDecoration(
+                        filled: true,
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: 12, horizontal: 12),
+                        enabledBorder: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(8)),
+                            borderSide:
+                                BorderSide(width: 2, color: Colors.white)),
+                        focusedBorder: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(8)),
+                            borderSide:
+                                BorderSide(width: 2, color: Colors.white)),
+                        fillColor: Colors.grey[200]),
+                    value: selectedItemSource,
+                    items: source
+                        .map(
+                          (item) => DropdownMenuItem<String>(
+                            value: item,
+                            alignment: AlignmentDirectional.centerEnd,
+                            child: Text(
+                              item,
+                              style: tabStyle,
+                            ),
+                          ),
+                        )
+                        .toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        selectedItemSource=value!;
+                      });
+                    },
+                  ),
+                )
               ],
             ),
           ],
